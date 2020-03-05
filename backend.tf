2bcd9b900c627bfc6e00ae4c83188821e7282b9e
Bishwa Karki <60760788+karkibishwa@users.noreply.github.com>
2020-03-04 20:28:25 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraform-docker-bishwa"
-    key    = "terraform/dev/ecs-bulbapp1"
+    key    = "terraform/dev/ecs-bulbapp"
     region = "us-east-1"
     profile = "bishwa"
   }
